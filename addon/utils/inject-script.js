05bfe1bafb94b47e307a741faa261cdb9ff14d8d
Gianni Settino <g@miltonandparc.com>
2017-05-02 18:49:35 -0400

    rejects promise on script load error

--- addon/utils/inject-script.js
+++ addon/utils/inject-script.js
@@ -1,12 +1,13 @@
 import Ember from 'ember';
 
 export default function injectScript(src) {
-  return new Ember.RSVP.Promise(function(resolve) {
-    var script    = document.createElement('script');
-    script.type   = 'text/javascript';
-    script.async  = true;
-    script.src    = src;
-    script.onload = function() { resolve(); };
+  return new Ember.RSVP.Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = src;
+    script.onload = resolve;
+    script.onerror = reject;
     document.getElementsByTagName('head')[0].appendChild(script);
   });
 }
